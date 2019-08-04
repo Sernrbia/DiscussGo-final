2170a6b465a91e3deaa71f0431684f43fc521dcf
Sernrbia <dejan.randjelovic.016@gmail.com>
2019-08-05 00:11:17 +0200

    fixed ui size

--- app/src/main/java/com/example/discussgo/MainActivity.java
+++ app/src/main/java/com/example/discussgo/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.discussgo;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTabHost;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
