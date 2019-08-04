6b1a3b7ed612ebe222cb68d9992323c2ca9f8780
Sernrbia <dejan.randjelovic.016@gmail.com>
2019-08-04 22:13:25 +0200

    fragments added, log in layout done, register layout 80% done

--- app/src/main/java/com/example/discussgo/MainActivity.java
+++ app/src/main/java/com/example/discussgo/MainActivity.java
@@ -8,26 +8,31 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTabHost;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment selectedFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            MenuItem otherItem;
             switch (item.getItemId()) {
                 case R.id.navigationLogin:
-                    Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_LONG).show();
-                    return true;
+                    selectedFragment = new LoginFragment();
+                    break;
                 case R.id.navigationRegister:
-                    Toast.makeText(MainActivity.this, "Register", Toast.LENGTH_LONG).show();
-                    return true;
+                    selectedFragment = new RegisterFragment();
+                    break;
             }
-            return false;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
         }
     };
 
@@ -35,10 +40,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ConstraintLayout cL = findViewById(R.id.constraintContent);
-//        cL.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if(selectedFragment == null) {
+            selectedFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
     }
 
 }
